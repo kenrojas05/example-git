17faecee1b680ecc8d39b79328c0553ab88038f0
unknown <curso@DESKTOP-CPO6BI4.ec.tec.ac.cr>
2024-09-06 16:27:14 -1200

    test
    New function added

--- example.py
+++ example.py
@@ -2,5 +2,9 @@ print("Hello world, my name is Isaac")
 
 def newFunction():
     print("Example Function")
+
+def function2():
+    print("Example function number 2")
+
 newFunction()
-    
+function2()
